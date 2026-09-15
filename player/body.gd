850c0ff089c7a2be94c9d6154292f414f1ae6703
CaptainCronch <62529272+CaptainCronch@users.noreply.github.com>
2026-09-15 01:11:18 -0300

    character x-axis flipping

--- player/body.gd
+++ player/body.gd
@@ -3,15 +3,35 @@ class_name Body
 
 @export var body_self_righting_torque := 5000000.0
 @export var body_self_righting_range := deg_to_rad(90.0)
+@export var flip := false
 
 var limbs: Array[Limb] = [null, null, null, null, null] ## [head, arm, leg, tail, back], from LimbStats.Slot enum.
 
 @export var body: RigidBody2D
+@export var polygon2d: Polygon2D
+@export var collider: CollisionPolygon2D
 @export var positions: Array[Marker2D] = [null, null, null, null, null]
 @export var joints: Array[RapierPinJoint2D] = [null, null, null, null, null]
 
 
 func _ready() -> void:
+	flip = bool(randi_range(0, 1))
+	if flip:
+		var polygon2d_polygon := polygon2d.polygon
+		for i in polygon2d.polygon.size():
+			polygon2d_polygon[i].x *= -1.0
+		polygon2d.polygon = polygon2d_polygon
+		polygon2d.position.x *= -1.0
+		
+		var collider_polygon := collider.polygon
+		for i in collider.polygon.size():
+			collider_polygon[i].x *= -1.0
+		collider.polygon = collider_polygon
+		collider.position.x *= -1.0
+		
+		for marker in positions:
+			marker.position.x *= -1.0
+	
 	for i in joints.size():
 		joints[i].position = positions[i].position
 		joints[i].node_a = joints[i].get_path_to(body)
@@ -33,6 +53,7 @@ func _physics_process(delta: float) -> void:
 
 func add_limb(limb_scene: PackedScene, limb_stats: LimbStats = null, bonus_stats: LimbStats = null) -> void:
 	var limb: Limb = limb_scene.instantiate()
+	if flip: limb.flip()
 	if is_instance_valid(limb_stats): limb.limb_stats = limb_stats
 	if is_instance_valid(bonus_stats): limb.bonus_stats = bonus_stats
 	var anchor_pos := positions[limb.limb_stats.slot].global_position
--- player/limb.gd
+++ player/limb.gd
@@ -7,10 +7,17 @@ class_name Limb
 
 var body: Body = null
 
-@export var limb_joint: RapierPinJoint2D ## This should be connected to the body (Node A).
-@export var bonus_joint: RapierPinJoint2D ## This should be connected to the main limb (Node A).
+@export var limb_joint: RapierPinJoint2D ## This should be connected to the body (Node A). Starts empty.
 @export var limb: RigidBody2D ## The limb which connects to the body.
+@export var limb_polygon: Polygon2D
+@export var limb_collider: CollisionPolygon2D
+
+var backwards := false
+
+@export var bonus_joint: RapierPinJoint2D ## This should be connected to the main limb (Node A).
 @export var bonus_limb: RigidBody2D ## The limb which connects to the main limb.
+@export var bonus_polygon: Polygon2D
+@export var bonus_collider: CollisionPolygon2D
 
 
 #func _ready() -> void:
@@ -19,16 +26,56 @@ var body: Body = null
 
 
 func flex(is_flexed: bool) -> void:
-	if not limb_stats.flip_orientation:
-		limb_joint.motor_position_target_angle = limb_stats.angular_limit_upper if is_flexed else limb_stats.angular_limit_lower
+	var direction := is_flexed != backwards # XOR
+	
+	if direction != limb_stats.flip_orientation:
+		limb_joint.motor_position_target_angle = limb_stats.angular_limit_upper
 	else:
-		limb_joint.motor_position_target_angle = limb_stats.angular_limit_lower if is_flexed else limb_stats.angular_limit_upper
+		limb_joint.motor_position_target_angle = limb_stats.angular_limit_lower
 	
 	if not is_instance_valid(bonus_limb): return
-	if not bonus_stats.flip_orientation:
-		bonus_joint.motor_position_target_angle = bonus_stats.angular_limit_upper if is_flexed else bonus_stats.angular_limit_lower
+	if direction != bonus_stats.flip_orientation:
+		bonus_joint.motor_position_target_angle = bonus_stats.angular_limit_upper
 	else:
-		bonus_joint.motor_position_target_angle = bonus_stats.angular_limit_lower if is_flexed else bonus_stats.angular_limit_upper
+		bonus_joint.motor_position_target_angle = bonus_stats.angular_limit_lower
+
+
+func flip() -> void:
+	backwards = not backwards
+	
+	limb.position.x *= -1.0
+	limb_polygon.polygon = reverse_points(limb_polygon.polygon)
+	limb_polygon.texture_offset.x *= -1.0
+	#limb_polygon.texture_rotation *= -1.0
+	limb_polygon.texture_scale.x *= -1.0
+	limb_polygon.position.x *= -1.0
+	limb_collider.polygon = reverse_points(limb_collider.polygon)
+	limb_collider.position.x *= -1.0
+	
+	var angle_holder := limb_stats.angular_limit_lower * -1.0
+	limb_stats.angular_limit_lower = limb_stats.angular_limit_upper * -1.0
+	limb_stats.angular_limit_upper = angle_holder
+	
+	if not is_instance_valid(bonus_limb): return
+	bonus_limb.position.x *= -1.0
+	bonus_polygon.polygon = reverse_points(bonus_polygon.polygon)
+	bonus_polygon.position.x *= -1.0
+	bonus_collider.polygon = reverse_points(bonus_collider.polygon)
+	bonus_collider.position.x *= -1.0
+	bonus_joint.node_b = ""
+	bonus_joint.position.x *= -1.0
+	bonus_joint.node_b = bonus_joint.get_path_to(bonus_limb)
+	
+	var bonus_holder := bonus_stats.angular_limit_lower * -1.0
+	bonus_stats.angular_limit_lower = bonus_stats.angular_limit_upper * -1.0
+	bonus_stats.angular_limit_upper = bonus_holder
+
+
+func reverse_points(points: PackedVector2Array) -> PackedVector2Array:
+	var copy := points.duplicate()
+	for i in copy.size():
+		copy[i].x *= -1.0
+	return copy
 
 
 static func setup_joint(pin_joint: RapierPinJoint2D, ## So you don't have to mess with the individual joints when creating limbs.
